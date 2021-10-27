f31a7d9909cc4837606629a5ebbc0c606c5e5237
Anartz Mugika Ledo <mugan86@gmail.com>
2021-10-27 20:36:39 +0200

    Preparando ejercicio

---
+++ practice-projects-material/section-2-ui-maps/01-markers-popup-tooltips/data/interest_points.ts
@@ -0,0 +1,34 @@
+export const interestPoints = [
+    {
+        lat: 43.308321,
+        lng: -1.977503,
+        img: 'https://raw.githubusercontent.com/leaflet-maps-course/resources/main/practice-projects-material/section-2-ui-maps/01-markers-popup-tooltips/img/amara-deusto.jpg',
+        name: 'Joan Antonio Aguirre Zubia',
+        info: ''
+    },
+    {
+        lat: 43.3245953,
+        lng: -1.9779,
+        img: 'https://raw.githubusercontent.com/leaflet-maps-course/resources/main/practice-projects-material/section-2-ui-maps/01-markers-popup-tooltips/img/kursaal.jpg',
+        name: 'Kursaal',
+        info: 'https://www.kursaal.eus/es/'
+    },
+    {
+        lat: 22,
+        lng: 33,
+        img: "",
+        name: ''
+    },
+    {
+        lat: 22,
+        lng: 33,
+        img: "",
+        name: ''
+    },
+    {
+        lat: 22,
+        lng: 33,
+        img: "",
+        name: ''
+    }
+]
