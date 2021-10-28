d8560007f680a8d8001e2029cead9c27a7201566
Anartz Mugika Ledo <mugan86@gmail.com>
2021-10-28 19:42:25 +0200

    Datos de sitios interesantes Donostia

--- practice-projects-material/section-2-ui-maps/01-markers-popup-tooltips/data/interest_points.ts
+++ practice-projects-material/section-2-ui-maps/01-markers-popup-tooltips/data/interest_points.ts
@@ -14,21 +14,24 @@ export const interestPoints = [
         info: 'https://www.kursaal.eus/es/'
     },
     {
-        lat: 22,
-        lng: 33,
-        img: "",
-        name: ''
+        lat: 43.3215846,
+        lng: -2.0076038,
+        img: 'https://raw.githubusercontent.com/leaflet-maps-course/resources/main/practice-projects-material/section-2-ui-maps/01-markers-popup-tooltips/img/peine-de-los-vientos.jpg',
+        name: 'Peine del viento',
+        info: 'https://www.sansebastianturismoa.eus/es/hacer/que-ver-san-sebastian/el-peine-del-viento'
     },
     {
-        lat: 22,
-        lng: 33,
-        img: "",
-        name: ''
+        lat: 43.3163723,
+        lng: -1.9882743,
+        img: "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/practice-projects-material/section-2-ui-maps/01-markers-popup-tooltips/img/laconcha.jpg",
+        name: "Playa La Concha",
+        info: "https://es.wikipedia.org/wiki/Playa_de_La_Concha_(San_Sebasti%C3%A1n)"
     },
     {
-        lat: 22,
-        lng: 33,
-        img: "",
-        name: ''
+        lat: 43.323549, 
+        lng: -1.988111,
+        img: "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/practice-projects-material/section-2-ui-maps/01-markers-popup-tooltips/img/puerto.jpg",
+        name: "Puerto",
+        info: ""
     }
 ]
